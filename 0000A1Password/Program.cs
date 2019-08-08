91850d164cdf29d1f752fb104ee7947614b57522
frank paulsen <frobnic+ita@gmail.com>
2019-08-08 08:38:53 +0200

    Projektdateien hinzufügen.

---
+++ 0000A1Password/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0000A1Password
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
